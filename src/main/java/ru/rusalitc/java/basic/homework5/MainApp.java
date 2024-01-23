6bc119ec20c1f61cfa3de43b1efb0472cc91ee3e
Stoker <berdnikov.vasyliy@gmail.com>
2024-01-23 16:49:13 +0800

    Version 1

--- src/main/java/ru/rusalitc/java/basic/homework5/MainApp.java
+++ src/main/java/ru/rusalitc/java/basic/homework5/MainApp.java
@@ -1,2 +1,39 @@
-package ru.rusalitc.java.basic.homework5;public class MainApp {
+package ru.rusalitc.java.basic.homework5;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Plate plate = new Plate(50);
+        Cat[] cats = {
+                new Cat("Барсик", 10),
+                new Cat("Барон", 20),
+                new Cat("Толстяк", 30),
+                new Cat("Мурзик", 15),
+                new Cat("Малыш", 5)
+        };
+
+        boolean fullnessCat = false;
+        while (!fullnessCat) {
+            int counterFalse = 0;
+            for (int i = 0; i < cats.length; i++) {
+                if (!cats[i].isFullness()) cats[i].toEat(plate);
+            }
+            for (int i = 0; i < cats.length; i++) {
+                cats[i].fullnessInfo();
+            }
+            for (int i = 0; i < cats.length; i++) {
+                if (!cats[i].isFullness()) {
+                    fullnessCat = false;
+                    counterFalse++;
+                }
+            }
+            if (counterFalse == 0) {
+                System.out.println("Все коты накормлены");
+                fullnessCat = true;
+            }
+            else {
+                plate.putFood();
+            }
+
+        }
+    }
 }
--- src/main/java/ru/rusalitc/java/basic/homework5/Plate.java
+++ src/main/java/ru/rusalitc/java/basic/homework5/Plate.java
@@ -1,2 +1,29 @@
-package ru.rusalitc.java.basic.homework5;public class Plate {
+package ru.rusalitc.java.basic.homework5;
+
+public class Plate {
+    private int maxFood;
+    private int curentFood;
+
+    public Plate(int maxFood) {
+        this.maxFood = maxFood;
+        this.curentFood = maxFood;
+    }
+
+    public int getCurentFood() {
+        return curentFood;
+    }
+
+    public void putFood() {
+        curentFood = maxFood;
+        System.out.println("Тарелку наполнили едой");
+    }
+
+    public boolean decreaseFood(int appetite) {// Не понял из задания как можно использовать метод  с boolean , тарелка не может уйти в минус, т.к. коты не будут есть если еды будет меньше их аппетита
+        curentFood -= appetite;
+        if (curentFood >= 0) return true;
+        else return false;
+
+
+
+    }
 }
